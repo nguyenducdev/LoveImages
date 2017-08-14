4e8e6e8fe99c4b60e2e40a701604301e406c5511
DucNguyen <thedie2509@gmail.com>
2017-08-14 15:32:04 +0700

    update css menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { Container, Header, Content, H1, H2, H3, Text, Button } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
@@ -8,12 +8,20 @@ export default class Menu extends Component {
     render() {
         return (
             <Container style={styles.container}>
-                <Button block info style={styles.button} onPress={Actions.M}>
-                    <Text>Categories</Text>
-                </Button>
-                <Button block info style={styles.button} onPress={Actions.M}>
-                    <Text>Color</Text>
-                </Button>
+                <View style={styles.header}>
+                    <Image
+                        style={{ width: 200, height: 100 }}
+                        source={{ uri: 'https://forsharing.info/logo_new.png' }}
+                    />
+                </View>
+                <View style={styles.body}>
+                    <Button block info style={styles.button} onPress={Actions.M}>
+                        <Text>Categories</Text>
+                    </Button>
+                    <Button block info style={styles.button} onPress={Actions.M}>
+                        <Text>Color</Text>
+                    </Button>
+                </View>
             </Container>
         );
     }
@@ -22,13 +30,23 @@ export default class Menu extends Component {
 var styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column'
+    },
+    header: {
+        flex: 1,
         flexDirection: 'column',
         justifyContent: 'center',
+        alignItems: 'center',
+    },
+    body: {
+        flex: 3,
+        flexDirection: 'column',
         alignItems: 'center',
     },
     button: {
         alignSelf: 'center',
         marginTop: 10,
-        width: 200
+        width: 200,
+        backgroundColor: '#000'
     }
 });
